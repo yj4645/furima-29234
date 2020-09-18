bc8c0ee2e3d37a2da8a77e44ac80301b3d9b3119
yj4645 <69782652+yj4645@users.noreply.github.com>
2020-09-18 17:14:20 +0900

    コードレビューの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,14 +13,24 @@ class ItemsController < ApplicationController
   end
 
   def create 
-    @item = Item.create(item_params)
-    redirect_to action: :index
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render :new
   end
+end
 
  private
 
   def item_params
     params.require(:item).permit(:user_id, :name, :price, :explanation, :category_id, :status_id, :burden_id, :prefecture_id, :shipping_days_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
 
