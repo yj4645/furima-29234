4c81f8416ba13ab4d98142ad710b8165475511f7
yj4645 <69782652+yj4645@users.noreply.github.com>
2020-09-18 19:41:19 +0900

    コードレビューの変更５

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,32 +36,32 @@ end
     end
 
     it 'カテゴリーが空では保存できない' do
-      @item.category_id = ''
+      @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank")
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
     it '商品の状態が空では保存できない' do
-      @item.status_id = ''
+      @item.status_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Status can't be blank")
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
     end
 
     it '配送料の負担が空では保存できない' do
-      @item.burden_id = ''
+      @item.burden_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Burden can't be blank")
+      expect(@item.errors.full_messages).to include("Burden must be other than 1")
     end
 
     it '発送元が空では保存できない' do
-      @item.prefecture_id = ''
+      @item.prefecture_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
     end
 
     it '発送までの日数が空で保存できない' do
-      @item.shipping_days_id = ''
+      @item.shipping_days_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping days can't be blank")
+      expect(@item.errors.full_messages).to include("Shipping days must be other than 1")
     end
 
     it '価格が空では保存できない' do
